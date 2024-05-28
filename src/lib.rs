13866a5de21fae07427e2c285fb0439ffe494d8a
jameswalstonn <james.walstonn@gmail.com>
2024-05-28 14:34:56 +0000

    i

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
-pub fn getrandom(_buf: &mut [u8]) -> Result<(), ()> {
+pub fn getrandom(_buf: &mut [u8]) -> Result<(), Error> {
     // Do nothing and return Ok
     Ok(())
 }
 
-mod getrandom {
-    #[derive(Debug)]
-    pub struct Error;
+#[derive(Debug)]
+pub struct Error;
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "getrandom error")
+    }
 }
+
+impl std::error::Error for Error {}
